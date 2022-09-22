a8e0037bcc925eca2b349c72b096cf97577282ae
Nick <efutin.kolya@yandex.ru>
2022-09-22 21:42:21 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+    
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        
+            break;
+    }
